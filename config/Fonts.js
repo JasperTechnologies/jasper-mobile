82fc5f28f92c66fd6c343f3e1661141ac8cbd304
15chrjef <15chrjef@gmail.com>
2019-09-17 21:12:31 -0700

    (update) landing screen

--- config/Fonts.js
+++ config/Fonts.js
@@ -2,5 +2,6 @@ export default {
   PoppinsRegular: require("../assets/fonts/PoppinsRegular.ttf"),
   PoppinsMedium: require("../assets/fonts/PoppinsMedium.ttf"),
   PoppinsSemiBold: require("../assets/fonts/PoppinsSemiBold.ttf"),
-  PoppinsBold: require("../assets/fonts/PoppinsBold.ttf")
+  PoppinsBold: require("../assets/fonts/PoppinsBold.ttf"),
+  LilitaOne: require("../assets/fonts/LilitaOne-Regular.ttf")
 }
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { StatusBar, StyleSheet, Text, Animated, Easing } from "react-native"
-import { connect } from 'react-redux';
+import { JASPER_INLINE_LOGO } from 'react-native-dotenv';
 import { useQuery } from '@apollo/react-hooks';
 import { yummy as screenTheme } from "../config/Themes"
 import {
@@ -10,7 +10,6 @@ import {
   withTheme,
   ScreenContainer,
   Container,
-  Button,
   View,
   Touchable,
   Image
@@ -41,20 +40,28 @@ function LandingContainer() {
   let scaleValue = new Animated.Value(0)
   const cardScale = scaleValue.interpolate({
     inputRange: [0, 0.25, .5, .75, 1],
-    outputRange: [1, 1.2, 1.4, 1.2, 1]
+    outputRange: [1, 1.3, 1.6, 1.3, 1]
   });
   startAnimation(scaleValue)
   return (
     <Container
       style={styles.Landing_Container}
     >
-      <View style={styles.Logo_View}>
-        <Image style={styles.Logo_Image} source={pictureURL} resizeMode="cover" />
+      <View style={styles.Jasper_Powered_Container}>
+        <Text style={styles.Jasper_Powered_Text}>
+          {`Powered By `}
+        </Text>
+        <Text style={styles.Jasper_Text}>
+          {` JASPER`}
+        </Text>     
       </View>
       <View style={styles.Welcome_Text_View}>
         <Text style={styles.Welcome_Text}>
-          {`Welcome to ${name}`}
+          {`Welcome To`}
         </Text>
+      </View>
+      <View style={styles.Logo_View}>
+        <Image style={styles.Logo_Image} source={pictureURL} resizeMode="contain" />
       </View>
       <Animated.View style={{...styles.TabTo_Text_View, transform: [{ scale: cardScale }] }}>
         <Animated.Text style={styles.TabTo_Text_Text}>
@@ -99,18 +106,40 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   Logo_View: {
-    height: 150,
-    width: 150
+    display: 'flex',
+    justifyContent: 'center',
+    width: "50%",
+    height: "30%"
   },
   Logo_Image: {
-    height: "100%",
-    width: "100%"
+    flex: 1,
+    height: undefined,
+    width: undefined,
   },
   Welcome_Text_View: {
-    paddingVertical: 24
+    paddingTop: "5%",
+    paddingBottom: 20
   },
   Welcome_Text: {
-    fontSize: 40
+    fontWeight: 'bold',
+    fontSize: 100
+  },
+  Jasper_Powered_Container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    top: "3%",
+    right: "3%",
+    position: 'absolute'
+  },
+  Jasper_Powered_Text: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  Jasper_Text: {
+    fontFamily: "LilitaOne",
+    fontSize: 30
   },
   TabTo_Text_View: {
     paddingVertical: 24
@@ -122,14 +151,3 @@ const styles = StyleSheet.create({
 })
 
 export default withTheme(LandingScreen);
-// const mapStateToProps = state => {
-//   return {
-//     logo: state.user.logo,
-//     name: state.user.name
-//   };
-// };
-//
-// const mapDispatchToProps = {
-// };
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(withTheme(LandingScreen));
